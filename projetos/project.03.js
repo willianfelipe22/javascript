fe20031cf829477ede9ee2735bb6086c2ade29bf
willianfelipe22 <felipewillian265@gmail.com>
2023-01-29 07:16:29 -0300

    projeto typeof e delete

---
+++ projetos/project.03.js
@@ -0,0 +1,33 @@
+                                            
+                                           //typeof e delete
+
+const myObject1 = {
+    name: "joão" ,
+    age: 20
+  }
+  
+  const myObject2 = {
+    name: "maria" ,
+    age: 40
+  }
+  
+  const myObject3 = {
+    name: "carlos" ,
+    age: 30
+  }
+   
+  delete myObject1.age 
+  delete myObject2.age
+  delete myObject3.age
+  
+  console.log(myObject1)
+  console.log(myObject2)
+  console.log(myObject3)
+  
+  const myName = "willian"
+  const myNumber = 33
+  const myObject = {
+    age: 22
+  }
+  
+  console.log(typeof myName)
