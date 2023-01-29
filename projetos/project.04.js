a0ff71eb699c8981564eb4eeb951ae5299d3f9f6
willianfelipe22 <felipewillian265@gmail.com>
2023-01-29 07:33:24 -0300

    projeto if / else

---
+++ projetos/project.04.js
@@ -0,0 +1,11 @@
+                                      // if e else
+const age = 17
+
+if(age >= 18){
+  console.log('a pessoa é maior de idade')
+}
+else{
+  console.log('a pessoa não é maior de idade')
+}
+
+console.log('terminou de executar meu codigo')
