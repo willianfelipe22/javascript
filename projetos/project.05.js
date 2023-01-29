a823b2e97224d4f2bc8c79b78135b70743abd423
willianfelipe22 <felipewillian265@gmail.com>
2023-01-29 07:54:04 -0300

    projeto if / else

---
+++ projetos/project.05.js
@@ -0,0 +1,18 @@
+/*controle de fluxo - condicionais - IF / ELSE
+
+if = SE
+else = SE NÃO
+
+*/
+
+
+const age = 18
+
+if(age >= 18){
+  console.log('a pessoa é maior de idade')
+}
+else{
+  console.log('a pessoa NÃO é maior de idade')
+}
+
+console.log('processo finalizado')
