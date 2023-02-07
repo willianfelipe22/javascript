5fa519b68ecefc026d0e18fffc9dc6ba90dd97fd
willianfelipe22 <felipewillian265@gmail.com>
2023-02-07 20:46:21 -0300

    switch case

---
+++ projetos/project.07.js
@@ -0,0 +1,27 @@
+/* CONTROLE DE FLUXO - CONDICIONAIS - SWITCH CASE */
+
+
+const temperature = 36
+
+switch(temperature){
+    case 30:
+    console.log('A pessoa esta com a temperatura muito abaixo do normal')
+    breack
+   
+    case 33:
+    console.log('a pessoa esta com atemperatura abaixo do normal')
+    breack
+    
+    case 36:
+    console.log('a pessoa esta com a temperatura normal')
+    breack
+    
+    case 40:
+    console.log('a pessoa esta com a temperatura a cima do normal')
+    brack
+    
+    default:
+    console.log('temperatura não encontrada')
+    breack
+    
+}
